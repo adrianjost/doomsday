7006dd945d9d6f92ebe0bf9d77f25a0fdcbe2a58
Adrian Jost <22987140+adrianjost@users.noreply.github.com>
2022-03-19 11:42:11 +0100

    ✨ confetti on success

--- src/components/QuestionPage.vue
+++ src/components/QuestionPage.vue
@@ -35,6 +35,7 @@ import PaperModal from "./PaperModal.vue";
 import { useLocalStorage } from "@vueuse/core";
 import { onBeforeMount, ref } from "vue";
 import StatisticsBar from "./StatisticsBar.vue";
+import confetti from "canvas-confetti";
 
 const props = defineProps({
   answer: { type: Number, required: true },
@@ -65,7 +66,8 @@ const pushStatistics = (attempts: number | "∞") => {
 const submit = (answer: number) => {
   attemptsForCurrentQuestion.value++;
   if (answer === props.answer) {
-    alert("✅ Correct! 🎉");
+    // alert("✅ Correct! 🎉");
+    confettiExplosion();
     emit("next");
     pushStatistics(attemptsForCurrentQuestion.value);
   } else {
@@ -75,6 +77,44 @@ const submit = (answer: number) => {
 const skip = () => {
   emit("next");
   pushStatistics(attemptsForCurrentQuestion.value === 0 ? 0 : "∞");
+};
+
+const confettiExplosion = () => {
+  var count = 150;
+  var defaults = {
+    origin: { y: 0.75 },
+  };
+
+  function fire(particleRatio: number, opts: Record<string, number>) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+      })
+    );
+  }
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  fire(0.2, {
+    spread: 60,
+  });
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
 };
 </script>
 
